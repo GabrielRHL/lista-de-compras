edb5730086c7b8e3f96022b88419ecd96f104b40
Gabriel <gabriell-rocha@hotmail.com>
2024-11-11 14:44:46 -0300

    alterando verificarCompraVazia.js

--- js/verificarCompraVazia.js
+++ js/verificarCompraVazia.js
@@ -1,7 +1,7 @@
 const mensagemCompraVazia = document.getElementById("mensagem-compra-vazia");
 
 export function verificarCompraVazia (lista) {
-    if (lista.childElementCount === 0) {
+    if (lista.querySelectorAll("li").length === 0) {
         mensagemCompraVazia.style.display = "block";
     } else {
         mensagemCompraVazia.style.display = "none";
